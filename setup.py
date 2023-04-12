bcb90da28a5562ddb60aa5216e4b3b3fdbda4bd6
ss391347 <sebastian.schwarz1@rwth-aachen.de>
2023-04-12 20:07:00 +0200

    Update 'package_data' in file setup.py.

--- setup.py
+++ setup.py
@@ -40,7 +40,7 @@ setuptools.setup(
     license="MIT",
     packages=setuptools.find_packages(where="src"),
     package_dir={"": "src"},
-    package_data={"pycity_scheduling": ["data/*.txt", "examples/*.ipynb"]},
+    package_data={"pycity_scheduling": ["data/*.txt", "data/*.dat", "examples/*.ipynb", "examples/case_studies/*.ipynb"]},
     data_files=[(".", ["LICENSE.txt", "README.md"])],
     install_requires=[
         "numpy==1.19.5",
